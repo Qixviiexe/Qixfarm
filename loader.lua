2845068d2c48d30eb25fe6ebab81e813737c8a9e
Qixviiexe <prohakerrs@gmail.com>
2026-02-14 20:37:19 +0100

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,12 @@
 local script_key = script_key or nil
-if getgenv.Qixfarmloaded then
+
+if getgenv().Qixfarmloaded then
     return
 end
-getgenv.Qixfarmloaded = true
+getgenv().Qixfarmloaded = true
 
-if game.PlaceId == "2788229376" then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Qixviiexe/Qixfarm/main/dahood.lua'))()
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Qixviiexe/Qixfarm/main/dahood.lua"))()
 else
-    print("Game not supported")
-    return
+    warn("Game not supported")
 end
